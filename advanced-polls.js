ff4c7bca8163a28bf250ad622670870fff0cf3fd
rewqjacky <79719140+rewqjacky@users.noreply.github.com>
2021-04-04 22:50:45 +0900

    Create advanced-polls.js

---
+++ advanced-polls.js
@@ -0,0 +1,17 @@
+const channels = ['827129069178978324']
+module.exports = (client) => {
+  client.on('message', (message) => {
+    const { channel, content } = message
+    if (!channels.includes(channel.id)) {
+      return
+    }
+    const eachLine = content.split('\n')
+    for (const line of eachLine) {
+      if (line.includes('=')) {
+        const split = line.split('=')
+        const emoji = split[0].trim()
+        message.react(emoji)
+      }
+    }
+  })
+}
