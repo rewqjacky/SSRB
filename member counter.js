756582836bcbb16b8ebadc3b5f551609d92c2d51
rewqjacky <79719140+rewqjacky@users.noreply.github.com>
2021-04-04 22:07:55 +0900

    Update member counter.js

--- member counter.js
+++ member counter.js
@@ -1,9 +1,9 @@
 module.exports = async (client) => {
-    const guild = client.guilds.cache.get('759720571721482273');
+    const guild = client.guilds.cache.get('716270880493404172');
     setInterval(() =>{
         const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('816667587567550486');
-        channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
+        const channel = guild.channels.cache.get('819164472089640970');
+        channel.setName(`群組人數: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count');
     }, 5000);
 }
